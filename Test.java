f21f9417ca8567713f8ab6d4bec12670153c9afb
Hyomin Yu <yhy0_myn00@naver.com>
2026-03-23 18:49:28 +0900

    hello world  자바 첫 시작

--- Test.java
+++ Test.java
@@ -1,5 +1,6 @@
 public class Test {
-    public static void main(String[] args) {
-        System.out.println("자바 세팅 완료! 코드 실행 성공!");
+    public static void main(String[] args)
+    {
+        System.out.println("hello, world!");
     }
 }
