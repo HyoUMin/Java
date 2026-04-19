52df7c43e0db564323978afae75d96bff722073f
Hyomin Yu <yhy0_myn00@naver.com>
2026-04-19 20:17:16 +0900

    super 사용법 추가

--- chap_07/camera/factory_cam.java
+++ chap_07/camera/factory_cam.java
@@ -17,6 +17,14 @@ public class factory_cam extends camera{// 자식 클래스로 사용할거임
     //     System.out.println("동영상을 녹화합니다.");
     // }
 
+    public void record_video() {
+        // 동영상 녹화
+        // 부모클래스의 record_vide를 실행한 후 detect_fire를 실행.
+        // 메소드 오버라이딩을 통해 기능을 변경하는 것이 아님.
+       super.record_video();
+       detect_fire();
+    }
+
     public void detect_fire() {
         // 화재 감지
         System.out.println("화재를 감지합니다.");
